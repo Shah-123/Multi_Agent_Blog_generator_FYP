84b568123d5bfecd8b6063168e8f0d3f970d7a73
Shah-123 <shahkarahmad342@gmail.com>
2026-01-29 15:07:10 +0500

    remove database and initialization scripts

--- Agents_backend/init_db.py
+++
@@ -1,6 +0,0 @@
-from database import init_db
-
-if __name__ == "__main__":
-    print("Creating database and tables...")
-    init_db()
-    print("Done! You should now see 'blog_factory.db' in your folder.")
